ad84fa9daf9ddb8fb3388c082ff8a82bc6db3647
Talha Khalid <42830771+talhakhaira@users.noreply.github.com>
2022-08-20 10:33:04 -0400

    Add files via upload

---
+++ Question2.js
@@ -0,0 +1,20 @@
+
+function checkPalindrome(string) {
+
+    const arrayValues = string.split('');
+
+    const reverseArrayValues = arrayValues.reverse();
+
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        console.log('It is a palindrome');
+    }
+    else {
+        console.log('It is not a palindrome');
+    }
+}
+
+const string = prompt('Enter a string: ');
+
+checkPalindrome(string);
