326f0e679620f5887511b98dc4e49e16408947db
Talha Khalid <42830771+talhakhaira@users.noreply.github.com>
2022-08-21 14:07:35 -0400

    Add files via upload

---
+++ Question8.js
@@ -0,0 +1,27 @@
+
+function testPrime(n)
+{
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+      return true;  
+  }
+}
+
+
+var num = prompt("Enter a number: ");
+
+alert((testPrime(num)));
